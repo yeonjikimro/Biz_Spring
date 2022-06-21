fde579cb2428165adfd7e873c1a119425dbd3a0d
yeonjikimro <duswlskfk42@naver.com>
2022-06-21 15:39:49 +0900

    6.21

--- SpMVC_007_EMS/src/main/java/com/callor/ems/model/EmsVO.java
+++ SpMVC_007_EMS/src/main/java/com/callor/ems/model/EmsVO.java
@@ -1,5 +1,17 @@
 package com.callor.ems.model;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,15 +28,43 @@ import lombok.ToString;
 public class EmsVO {
 
 	private long e_seq;
+	
 	private String e_from_email;
+	
+	@Email(message = "Email 형식이 아닙니다")
+	@NotEmpty(message = "받는 사람 Email 은 반드시 입력하세요")
 	private String e_to_email;
 	
+	@NotEmpty(message = "보내는 사람 이름은 null 일 수 없습니다")
 	private String e_from_name;
+	
+	@Length(min = 1, max = 100, message = "받는 사람 이름은 1글자 ~ 100글자까지만 가능")
+	@NotEmpty(message = "받는 사람 이름은 반드시 입력하세요")
 	private String e_to_name;
 	
+	@NotEmpty(message = "제목을 입력해야 합니다")
 	private String e_subject;
-	private String e_content;	
+	private String e_content;
+	
+	
+	// @Size(min = 1, max = 10, message = "num 는 1부터 10사이 값이어야 합니다")
 	private String e_send_date;
 	private String e_send_time;
+	
+	// @Min(value=1, message = "num 는 1보다 커야합니다")
+	// @Max(value=10, message = "num 는 10보다 작아야 합니다")
+	
+	private int num;
+	
+	// @DecimalMin(value="1", message = "decimal 은 십진수 1보다 커야 합니다")
+	// @DecimalMax(value="10", message = "decimal 은 십진수 10보다 작아야 합니다")
+	private int decimal;
+	
+	// @Pattern(regexp = "^.*(?=^.{8,15}$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$")
+	private String password;
+	
+	
+	
+	
 	
 }
--- SpMVC_007_EMS/src/main/webapp/WEB-INF/views/home.jsp
+++ SpMVC_007_EMS/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,45 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://www.springframework.org/tags/form"  prefix="form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<style>
+	section.main {
+		width: 70%;
+		margin: 10px auto;
+	}
+	form#emsVO input, form#emsVO button {
+		width: 90%;
+	}
+	span {
+		color:red;
+	}
+</style>
 </head>
 <body>
-
-<h1>반갑습니다</h1>
+	<section class="main">
+		<form:form modelAttribute="emsVO">
+			
+			<form:input path="e_to_email" placeholder="받는 Email" /><br/>
+			<form:errors path="e_to_email"/>
+			
+			<form:input path="e_from_name" placeholder="보내는 사람 이름"/><br/>
+			<form:errors path="e_from_name"/>
+			
+			<form:input path="e_to_name" placeholder="받는 사람 이름"/><br/>
+			<form:errors path="e_to_name"/>
+			
+			<form:input path="e_subject" placeholder="제목"/><br/>
+			<form:errors path="e_subject"/>
+			
+			<form:input path="e_content" placeholder="내용"/><br/>
+			<button>보내기</button>
+		</form:form>
+	</section>
 
 </body>
 </html>
